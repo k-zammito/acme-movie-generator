810a3abd61c8d7bf3e5482c1984c1e882fd69d67
k-zammito <kzammito@gmail.com>
2021-12-31 15:40:58 -0500

    added average rating to nav

--- src/Nav.js
+++ src/Nav.js
@@ -2,11 +2,25 @@ import React from 'react';
 import MovieButton from './MovieButton';
 
 const Nav = (props) => {
+  console.log('NAV -->', props.movies);
   return (
     <nav>
       <h3>ACME Movies</h3>
       <MovieButton />
       <h4>Movie Count ({props.movies.length})</h4>
+      <h4>
+        Average Rating (
+        {props.movies.length === 0
+          ? 0
+          : Math.round(
+              (props.movies
+                .map((movie) => movie.rating)
+                .reduce((a, b) => a + b, 0) /
+                props.movies.length) *
+                10
+            ) / 10}
+        )
+      </h4>
     </nav>
   );
 };
